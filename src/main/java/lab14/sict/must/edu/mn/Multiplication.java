ad6018462dd881ed276d31b43e0b7d1b2f25e390
B222270066 <ankhbayrtuvshinjargal@gmail.com>
2025-12-09 22:57:48 +0800

    Resolve merge conflict by applying release version of Multiplication

--- src/main/java/lab14/sict/must/edu/mn/Multiplication.java
+++ src/main/java/lab14/sict/must/edu/mn/Multiplication.java
@@ -1,7 +1,15 @@
 package lab14.sict.must.edu.mn;
 
 public class Multiplication {
-    public double multiply(double a, double b) {
+
+    public double multiply(int a, int b) {
+
+        // Extra branch for Jacoco: validate big input
+        if (a > 10000 || b > 10000) {
+            throw new IllegalArgumentException("Input too large");
+        }
+
+        // Normal multiplication
         return a * b;
     }
 }
